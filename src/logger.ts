5dabe2c89695041fb624f58e4206f8f591ca40d6
zhongyangxun <zhongyangxun@gmail.com>
2026-07-30 12:03:23 +0800

    feat(logger): add custom logger and install chalk

---
+++ src/logger.ts
@@ -0,0 +1,30 @@
+import chalk from 'chalk'
+
+type logger = {
+  start: (message: string) => void
+  success: (message: string) => void
+  info: (message: string) => void
+  error: (message: string) => void
+}
+
+const loggerIcons = {
+  start: '🚀',
+  success: '✨',
+  info: '💡',
+  error: '✖',
+}
+
+export const logger: logger = {
+  start: (message: string) => {
+    console.log(chalk.cyan(`${loggerIcons.start} ${message}`))
+  },
+  success: (message: string) => {
+    console.log(chalk.green(`${loggerIcons.success} ${message}`))
+  },
+  info: (message: string) => {
+    console.log(chalk.blue(`${loggerIcons.info} ${message}`))
+  },
+  error: (message: string) => {
+    console.log(chalk.red(`${loggerIcons.error} ${message}`))
+  },
+}
